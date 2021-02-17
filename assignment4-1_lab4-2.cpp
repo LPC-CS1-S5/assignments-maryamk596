400c6a7bdbf060fd03c3b69816353969b87dd28d
maryamk596 <78011368+maryamk596@users.noreply.github.com>
2021-02-16 23:31:19 -0800

    Update assignment4-1_lab4-2.cpp

--- assignment4-1_lab4-2.cpp
+++ assignment4-1_lab4-2.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std; 
 
+
+int main ()
+{
+  const double w02= 1.10;
+  const double w06 = 2.20;
+  const double w10 = 3.70; 
+  const double w20 = 4.80;
+  double RATE, weight, distance;
+  double total_price;
+  
+  cin >> weight >> distance ; 
+  
+  if ((weight < 0) || (weight > 20))
+    exit (0);
+  
+  
+  if (weight <= 2) 
+    RATE = w02;
+  else if (weight < 6) // 2-6
+    RATE = w06; 
+  else if (weight < 10) 
+    RATE = w10;
+  else if (weight < 20) 
+    RATE = w20;
+  
+  if ((distance < 10) || (distance > 3000))
+    exit (0);
+  
+   if (distance > 500) 
+   total_price = distance / 500 * RATE; 
+  cout <, " Total price: " total_price << endl;cout << "Weight: " weightt << endl; 
+  cout  << "Distance: " distance << endl; 
+}
