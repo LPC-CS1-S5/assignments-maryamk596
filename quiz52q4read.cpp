7383e9e84e826537f22b5212ccb5818840c55fbf
MARYAM MOHSIN <mxmohsin@zonemail.clpccd.edu>
2021-03-11 07:20:38 +0000

    commit by maryam

---
+++ quiz52q4read.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std; 
+
+int main()
+{ 
+  ifstream ifs; 
+
+  int num;
+  string nam; 
+
+  ifs.open("employee.txt");
+  if (ifs.fail())
+  {
+    cerr << "File open error\n";
+    exit (0); 
+  }
+
+  int i = 0; 
+  while (ifs >> name)
+  {
+    ifs >> name; 
+    i++; 
+  }
+
+  ifs.close();
+}
