58afed20a426919a2628506aa78298c2804c9fa5
MARYAM MOHSIN <mxmohsin@zonemail.clpccd.edu>
2021-04-01 06:35:09 +0000

    commit by maryam

---
+++ midterm.cpp/midtermq5.cpp
@@ -0,0 +1,11 @@
+// Make your program that asks for two user input num1 and num2 (integer) and calculate the integer division n1/n2 if n1 > n2. Otherwise n2/n1. And then, the result must be written into the file "question5.txt". If the two numbers are the same, we will stop the program.
+
+//Make the function getInput to take two user input
+//Make the function isSame to check two numbers are the same. This function could be used as an exit condition of the loop structure.
+//Make the function intDivision to calculate the division.
+//Make the function fileWrite to write the division result to a file
+//Make the main function to drive all these functions
+//All other considerations for designing the programs can be set by your algorithms and your design specification.
+
+//the number of parameters, parameter data type, return value and type, and so on.
+#include <iostream> 
