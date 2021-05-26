c6f7978c03be78aa92fd36dda6a0e639ffd570dc
MARYAM MOHSIN <>
2021-05-26 16:45:02 +0000

    commit 9:44

--- Final.cpp/finalq5.cpp
+++ Final.cpp/finalq5.cpp
@@ -8,8 +8,25 @@ struct Scores{
   double sc[N];
   string grade;
 };
-struct Grade{
+struct D- {
   string Aloysius;
   string Alflfa;
   string 123-45-6789
+  Scores 40.0, 90.0, 100.0, 83.0, 49.0
+};
+Grade D-;
+void printstruct (D-);
+int main()
+{
+  ifstream ifs;
+  
+  ifs.open ("grades.bin");
+  if (! ifs ){
+    cout << "File Open Error : Sure that there is the file grades.bin \n";
+    exit (0);
+  }
+
+  while ( ifs.read((char *)&g, sizeof(g))){
+    printstruc
+  }
 }
