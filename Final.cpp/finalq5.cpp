cbe953f83397a3214806bc3ae8c33ac35db54d8b
MARYAM MOHSIN <>
2021-05-26 17:18:12 +0000

    commit 10:17

--- Final.cpp/finalq5.cpp
+++ Final.cpp/finalq5.cpp
@@ -43,6 +43,115 @@ struct B- {
 };
 Grade B-;
 
+
+struct A- {
+  string Fred;
+  string Bumpkin;
+  string 456-78-9012
+  Scores 43.0,78.0,88.0,77.0,45.0
+};
+Grade A-;
+
+
+struct C- {
+  string Betty;
+  string Rubble;
+  string 234-56-7890
+  Scores 44.0,90.0,80.0,90.0,46.0
+};
+Grade C-;
+
+
+struct F {
+  string Cecil;
+  string Noshow;
+  string 345-67-8901
+  Scores 45.0,11.0,-1.0,4.0,43.0
+};
+Grade F;
+
+
+struct B+ {
+  string Bif;
+  string Buff;
+  string 632-79-9939
+  Scores 46.0,20.0,30.0,40.0,50.0
+};
+Grade B+;
+
+
+struct A {
+  string Andrew;
+  string Airpump;
+  string 223-45-6789
+  Scores 49.0,1.0,90.0,100.0,83.0
+};
+Grade A;
+
+
+struct A+ {
+  string Jim;
+  string Backus;
+  string 143-12-1234
+  Scores 48.0,1.0,97.0,96.0,97.0
+};
+Grade A+;
+
+
+struct D+ {
+  string Art;
+  string Carnivore;
+  string 565-89-0123
+  Scores 44.0,1.0,80.0,60.0,40.0
+};
+Grade D+;
+
+
+struct C+ {
+  string Jim;
+  string Dandy;
+  string 087-75-4321
+  Scores 47.0,1.0,23.0,36.0,45.0
+};
+Grade C+;
+
+
+struct B- {
+  string Ima;
+  string Elephant;
+  string 456-71-9012
+  Scores 45.0,1.0,78.0,88.0,77.0
+};
+Grade B-;
+
+
+struct B- {
+  string Benny;
+  string Franklin;
+  string 234-56-2890
+  Scores 50.0,1.0,90.0,80.0,90.0
+};
+Grade B-;
+
+
+struct B {
+  string Boy;
+  string George;
+  string 345-67-3901
+  Scores 40.0,1.0,11.0,-1.0,4.0
+};
+Grade B;
+
+
+struct C {
+  string Harvey;
+  string Heffalump;
+  string 632-79-9439
+  Scores 30.0,1.0,20.0,30.0,40.0
+};
+Grade C;
+
+
 void printstruct (D-);
 int main()
 {
