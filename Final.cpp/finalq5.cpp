34d176f661f6c6c0b97e85ffda9325c5b4b11f58
MARYAM MOHSIN <>
2021-05-26 16:30:11 +0000

    commit by maryam

---
+++ Final.cpp/finalq5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+const unsigned short N=5;
+struct Scores{
+  double sc[N];
+  string grade;
+};
+struct Grade{
+  string Aloysius;
+  string Alflfa;
+  string 123-45-6789
+}
